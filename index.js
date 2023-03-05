b2304dbd3129dda08e3f25c868ff715e3eb5c90b
iamabj1996 <ab02.joseph@gmail.com>
2023-03-05 15:31:38 +0530

    push 5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Web3 = require('web3');
 const app = express();
 
 
-//adding bodypaser to get data in the output
+//adding bodypaser to get data in the outputs
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: false, limit:'50mb'}));
 
