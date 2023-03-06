2a602341623cedf5a39c69a6de92d76d3e836ab2
iamabj1996 <ab02.joseph@gmail.com>
2023-03-06 07:59:25 +0530

    new pushes

--- routes/api/owner.js
+++ routes/api/owner.js
@@ -83,10 +83,10 @@ router.get('/getEncyrptedData/:appName/:scriptType/:callerAccountAddress/:releas
 
 router.post('/storeTableData', async(req,res)=>{
     const {callerPrivateKey, appName, tableName, typeOfScript, releaseVersion, encryptedData, callerAccountAddress} = req.body
+    console.log('body', req.body);
     console.log('encryptedData', encryptedData);
-    const stringData = JSON.parse(encryptedData)
     const finalEncryptedData= [];
-    stringData.map((data)=>{
+    encryptedData.map((data)=>{
        let newData = {sys_id: data.sys_id , script: SHA256(data.script + data.sys_id).toString()};
        finalEncryptedData.push(JSON.stringify(newData));
     })
