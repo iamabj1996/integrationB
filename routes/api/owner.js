120c630a03b0025bb0f0ebbcd563041052ee4e57
iamabj1996 <ab02.joseph@gmail.com>
2023-03-16 17:37:56 +0530

    hhh

--- routes/api/owner.js
+++ routes/api/owner.js
@@ -2,27 +2,40 @@ const { SHA256 } = require('crypto-js');
 const express = require('express');
 const router = express.Router();
 const Web3 = require('web3');
+const http = require('http');
 const { address, abi } = require('../../smartContract/Table');
+const options ={
+    keepAlive : true,
+    withCredentials: false,
+    timeout: 20000,
+    headers: [
+        {
+            name: 'Access-Control-Allow-Origin',
+            value: '*'
+        }
+    ],
+    agent: {
+        http: http.Agent(),
+        baseUrl: ''
+    }
+}
+const web3Provider = new Web3(new Web3.providers.HttpProvider("http://192.168.29.71:8545",options ));
 
+const tokenContract = new web3Provider.eth.Contract(
+    abi,
+    address
+    );
 
 router.post('/add_client', async(req,res) =>{
 
 
     
-  
+    const {callerPrivateKey , clientAddress , callerAccountAddress} = req.body;
+    console.log(req.body);
+    // import wallet in the provider using private key of owner
+    web3Provider.eth.accounts.wallet.add(callerPrivateKey);
+   
     try {
-        const httpProvider = "http://192.168.29.71:8545/"
-        const web3Provider = new Web3(new Web3.providers.HttpProvider(httpProvider));
-        
-        const tokenContract = new web3Provider.eth.Contract(
-            abi,
-            address
-            );
-        const {callerPrivateKey , clientAddress , callerAccountAddress} = req.body;
-        console.log(req.body);
-        // import wallet in the provider using private key of owner
-        await web3Provider.eth.accounts.wallet.add(callerPrivateKey);
-       
         console.log('adding client')
     
         // 1 create smart contract transaction
