df5ff9eba177b1bba25bf7c03bc663810b4d6ed4
iamabj1996 <ab02.joseph@gmail.com>
2023-03-16 16:39:38 +0530

    pushes of cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const Web3 = require('web3');
+const cors = require('cors');
 
 const app = express();
 
 
 //adding bodypaser to get data in the outputs
+app.use(cors({
+    origin:'*',
+}))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: false, limit:'50mb'}));
 
