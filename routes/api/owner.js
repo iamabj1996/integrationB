6a6a0ce986e6cdaabbdc1df99d32d31291880390
iamabj1996 <ab02.joseph@gmail.com>
2023-10-19 19:19:24 +0530

    bb

--- routes/api/owner.js
+++ routes/api/owner.js
@@ -163,12 +163,12 @@ router.post('/storeTableData', async (req, res) => {
 			2,
 			2
 		);
-		// 2 calculate gas fee
-		// const gas = await trx.estimateGas({ from: callerAccountAddress });
-		// console.log('gas :>> ', gas);
-		// // 3 calculate gas price
-		// const gasPrice = await web3Provider.eth.getGasPrice();
-		console.log('gasPrice :>> ', gasPrice);
+		// // 2 calculate gas fee
+		// // const gas = await trx.estimateGas({ from: callerAccountAddress });
+		// // console.log('gas :>> ', gas);
+		// // // 3 calculate gas price
+		// // const gasPrice = await web3Provider.eth.getGasPrice();
+		// console.log('gasPrice :>> ', gasPrice);
 		// 4 encode transaction data
 		const data = trx.encodeABI();
 		console.log('data :>> ', data);
